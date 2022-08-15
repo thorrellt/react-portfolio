51d996f8587d7c3ce16e7b70d11083b7e1c79779
Thorrell Turner <thorrellt@gmail.com>
2022-08-15 09:34:27 -0400

    add offcanvas to nav

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,28 +2,42 @@ import { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import '../styles/MainNav.css'
 
 function MainNav() {
+    const expand = "lg"
     return (
+
         <Navbar collapseOnSelect expand="lg" id="MainNav" fixed="top" bg="primary-transparent" variant="dark">
             <Container className="pe-3">
-            <Navbar.Brand href="#"></Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link
-                            href="#"
-                            className="mainNav--link me-1">About Me</Nav.Link>
-                        <Nav.Link
-                            href="#"
-                            className="mx-4">Projects</Nav.Link>
-                        <Nav.Link
-                            href="#"
-                            className="ms-1">Contact Me</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+                <Navbar.Brand href="#"></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Offcanvas
+                    id={`offcanvasNavbar-expand-${expand}`}
+                    aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                    placement="end"
+                    className="bg-secondary"
+                >
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                            thorrellt.com
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="ms-auto">
+                            <Nav.Link
+                                href="#"
+                                className="mx-2">About Me</Nav.Link>
+                            <Nav.Link
+                                href="#"
+                                className="mx-2">Projects</Nav.Link>
+                            <Nav.Link
+                                href="#"
+                                className="mx-2">Contact Me</Nav.Link>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
 
             </Container>
         </Navbar>
