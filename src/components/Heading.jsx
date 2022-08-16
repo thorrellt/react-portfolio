f7c90a236de738cdda11086d2d62d22b140ffe25
Thorrell Turner <thorrellt@gmail.com>
2022-08-16 08:58:41 -0400

    add resume and download btn

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import '../scss/Heading.scss'
+import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image'
+import '../scss/Heading.scss'
 import profilePicture from '../assets/img-profile-picture.png'
+import resume from '../assets/thorrellt_resume.pdf'
 
 function Heading() {
 
   return (
-    <Container fluid>
+    <Container className = ".Heading" fluid>
       <Row>
-        <div id="heading--left" className="col-6 bg-secondary">1 of 2</div>
+        <div id="heading--left" className="col-6 bg-secondary"></div>
         <div id="heading--right" className="col-6 bg-primary">
           <Image
             className="profile-pic ms-5 me-5"
@@ -27,7 +29,14 @@ function Heading() {
         <p>a Software Developer based<br/>
           out of the Washington<br/>
           Metropolitan area</p>
-          
+
+        <Button variant="danger" size="lg" 
+        className='px-3 py-3'
+        href={resume}
+        download="ThorrellT_Resume">
+          <strong>DOWNLOAD CV</strong>
+        </Button>
+
       </div>
     </Container>
   );
