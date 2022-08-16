dec4d7e669aab62f72560adfb7a63dee05ccda4c
Thorrell Turner <thorrellt@gmail.com>
2022-08-16 08:15:55 -0400

    add image and intro to Heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,18 +2,35 @@ import { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import '../styles/Heading.css'
+import '../scss/Heading.scss'
+import Image from 'react-bootstrap/Image'
+import profilePicture from '../assets/img-profile-picture.png'
 
 function Heading() {
-    
-    return (
-        <Container fluid>
-          <Row>
-            <div id="heading--left" className="col-6 bg-secondary">1 of 2</div>
-            <div id="heading--right" className="col-6 bg-primary">hello world</div>
-          </Row>
-        </Container>
-      );
-  }
-  
-  export default Heading
+
+  return (
+    <Container fluid>
+      <Row>
+        <div id="heading--left" className="col-6 bg-secondary">1 of 2</div>
+        <div id="heading--right" className="col-6 bg-primary">
+          <Image
+            className="profile-pic ms-5 me-5"
+            src={[profilePicture]}
+            fluid
+            alt="Thorrell T Profile Pic"
+          />
+        </div>
+      </Row>
+      <div className="intro position-absolute top-45 start-45 my-0 translate-middle">
+        <p>hello world, I'm</p>
+        <h1 className='display-4'>Thorrell <span class="text-danger">T</span>urner</h1>
+        <p>a Software Developer based<br/>
+          out of the Washington<br/>
+          Metropolitan area</p>
+          
+      </div>
+    </Container>
+  );
+}
+
+export default Heading
