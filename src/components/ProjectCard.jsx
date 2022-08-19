938729307aa33f53aebe6cbfd0032d82ee06e9df
Thorrell Turner <thorrellt@gmail.com>
2022-08-19 09:13:48 -0400

    update img loading logic for project cad

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,12 +6,22 @@ import Card from 'react-bootstrap/Card';
 
 
 export default function ProjectCard(props) {
-    const publicPath = 'react-portfolio/'
-    const assetPath = '../assets/'
+    const imgName = 'img-api-banner.jpeg'
+    const devImgPath = `/${imgName}`
+    const liveImgPath = `/react-portfolio/${imgName}`
+    
+    function ImageExist(url) {
+        var img = new Image();
+        img.src = url;
+        return img.height != 0;
+    }
+    const imgPath = ImageExist(devImgPath) ? devImgPath : liveImgPath
+    
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Img variant="top" src="/img-api-banner.jpeg" />
+            <Card.Body className='p-0'>
+                <Card.Img variant="top"
+                    src={imgPath} />
                 <Card.Title>Card Title</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
             </Card.Body>
