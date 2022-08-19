ed85a0987dd366dc0ba13eaa74a8169343c10ada
Thorrell Turner <thorrellt@gmail.com>
2022-08-19 08:49:28 -0400

    update card logic

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,10 +4,14 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
+
 export default function ProjectCard(props) {
+    const publicPath = 'react-portfolio/'
+    const assetPath = '../assets/'
     return (
         <Card style={{ width: '18rem' }}>
             <Card.Body>
+                <Card.Img variant="top" src="/img-api-banner.jpeg" />
                 <Card.Title>Card Title</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
             </Card.Body>
